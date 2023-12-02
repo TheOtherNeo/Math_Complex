d551c874752069531f3126083f85f622d426176c
Neil Bezuidenhout <34944157+TheOtherNeo@users.noreply.github.com>
2023-12-02 12:16:10 +0400

    Added sum() function.

--- Math_Complex.php
+++ Math_Complex.php
@@ -21,7 +21,7 @@ namespace Math_Complex{
          * @param string $suffix
          * Suffix to denominate the imaginary number as i or j
          */
-        public function __construct(?float $real = 0, ?float $im = 0, ?string $suffix = "i",)
+        public function __construct( float $real = 0, float $im = 0, string $suffix = "i",)
         {
             $this->real = $real;
             $this->im = $im;
@@ -40,7 +40,7 @@ namespace Math_Complex{
             ];
         }
 
-        public function __unserialize(?array $data): void
+        public function __unserialize( array $data ): void
         {
             $this->real = $data["real"];
             $this->im = $data["im"];
@@ -55,6 +55,22 @@ namespace Math_Complex{
                 return (string) $this->real." - ".abs($this->im).$this->suffix;
             }
         }
+
+        /**
+         * Returns the imaginary part of the complex number
+         */
+        public function getIm():float
+        {
+            return $this->im;
+        }
+
+        /**
+         * Returns the real part of the complex number
+         */
+        public function getReal():float
+        {
+            return $this->real;
+        }
     } // end - class Math_Complex
 
     class Math_ComplexOp extends Math_Complex {
@@ -65,7 +81,7 @@ namespace Math_Complex{
          * @param Math_Complex &$c2
          * @return Math_Complex $z
          */ 
-        public static function add( ?Math_Complex &$c1 , ?Math_Complex &$c2, ): Math_Complex
+        public static function add( Math_Complex &$c1 , Math_Complex &$c2, ): Math_Complex
         {
             $z = new Math_Complex();
             
@@ -78,7 +94,7 @@ namespace Math_Complex{
          * @param Math_Complex &$c1
          * @return Math_Complex $z
          * */ 
-        public static function conjugate( ?Math_Complex &$c1, ): Math_Complex
+        public static function conjugate( Math_Complex &$c1, ): Math_Complex
         {
             // $z = unserialize(serialize($c1));
             // Create a shallow copy of the supplied object
@@ -96,7 +112,7 @@ namespace Math_Complex{
          * Angle in radians
          * @return Math_Complex $z
          */
-        public static function createFromPolar( ?float $r , ?float $theta, ): Math_Complex
+        public static function createFromPolar( float $r , float $theta, ): Math_Complex
         {
             $z = new Math_Complex();
             // Remember that theta is in radians, not degrees. So use deg2rad in normal use.
@@ -113,7 +129,7 @@ namespace Math_Complex{
          * @param Math_Complex &$c2
          * @return Math_Complex $z
          */
-        public static function mult( ?Math_Complex &$c1 , ?Math_Complex &$c2, ): Math_Complex
+        public static function mult( Math_Complex &$c1 , Math_Complex &$c2, ): Math_Complex
         {
             $z = new Math_Complex();
             // (a+bi)(c+di) = (ac−bd) + (ad+bc)i
@@ -127,7 +143,7 @@ namespace Math_Complex{
          * @param float $real
          * @return Math_Complex $z
          *  */        
-        public static function multReal( ?Math_Complex &$c1 , ?float $real, ): Math_Complex
+        public static function multReal( Math_Complex &$c1 , float $real, ): Math_Complex
         {
             $z = new Math_Complex();
             // No need for special calculations, just multiply each item with the value
@@ -135,5 +151,32 @@ namespace Math_Complex{
             $z->im = $real * $c1->im;
             return $z;
         }
-    }
+    } //class Math_ComplexOp extends Math_Complex
+
+    /**
+     * Functions that are not part of the PEAR library, but useful in relation to Excel functions
+     */
+    class Math_ComplexEx extends Math_ComplexOp {
+
+        /**
+         * Returns the sum of two or more complex numbers in x + yi or x + yj
+         */
+        public static function sum(...$complex): Math_Complex
+        {
+            // Object into which values will be placed.
+            $z = new Math_Complex();
+            // Ensuring that the start values are zero.
+            $z->real = 0;
+            $z->im = 0;
+
+            // First need to seperate the real from the complex numbers
+            foreach ($complex as $k => $v) {
+                $z->real += $v->getReal();
+                $z->im += $v->getIm();
+            }
+
+            // Retrun the sum product as a new complex number
+            return $z;
+        } //public static function sum
+    } // class Math_ComplexEx extends Math_ComplexOp
 } // Namespace
